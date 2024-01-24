4a123996edcafd6b6281d32f823c09b813b8aa7f
Chandram-Dutta <chandramdutta2004@gmail.com>
2024-01-25 02:13:29 +0530

    Initial Commit

---
+++ functions/index.js
@@ -0,0 +1,22 @@
+var functions = require('firebase-functions');
+var admin = require('firebase-admin');
+var axios = require('axios');
+
+admin.initializeApp();
+
+exports.onUserCreated = functions.auth.user().onCreate(function (user) {
+    var url = 'https://suzi-backend.onrender.com/login';
+    var data = {
+        email: user.email,
+        firebaseID: user.uid,
+        state: 'india',
+    };
+
+    return axios.post(url, data)
+        .then(function (response) {
+            functions.logger.info('Post request sent', { structuredData: true });
+        })
+        .catch(function (error) {
+            functions.logger.error('Error sending post request', { structuredData: true });
+        });
+});
