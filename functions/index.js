f259c459d8b64e163d08ccefc4957f118ab7d148
Chandram-Dutta <chandramdutta2004@gmail.com>
2024-02-20 12:09:18 +0530

    feat: updated url

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ var axios = require('axios');
 admin.initializeApp();
 
 exports.onUserCreated = functions.auth.user().onCreate(function (user) {
-    var url = 'https://suzi-backend.onrender.com/login';
+    var url = 'https://suzi-backend-kunqmukjgq-uw.a.run.app/';
     var data = {
         email: user.email,
         firebaseID: user.uid,
